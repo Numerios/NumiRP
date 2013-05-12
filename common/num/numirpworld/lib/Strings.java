2935836f4b8155ba875de7441c9af15b10a85517
Numerios <mrnumerios@gmail.com>
2013-05-12 19:49:48 +0200

    Preparing for ores

---
+++ common/num/numirpworld/lib/Strings.java
@@ -0,0 +1,7 @@
+package num.numirpworld.lib;
+
+public class Strings {
+    public static final String[] ORES = { "Sapphire", "GreenSapphire", "Ruby",
+            "Copper", "Tin", "Silver", "Nikolite", "Tungsten" };
+
+}
