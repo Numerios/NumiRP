904d578580d3f2656887f1f48e7f5fa988b6c752
Numerios <mrnumerios@gmail.com>
2013-05-21 21:18:36 +0200

    Changed all metadata to be equivalent to the RP2 ones

--- common/num/numirpworld/lib/Strings.java
+++ common/num/numirpworld/lib/Strings.java
@@ -1,13 +1,15 @@
 package num.numirpworld.lib;
 
 public class Strings {
-    public static final String[] ORES = { "Sapphire", "GreenSapphire", "Ruby",
-            "Copper", "Tin", "Silver", "Nikolite", "Tungsten" };
-    public static final String[] ORE_NAMES = { "Sapphire", "Green Sapphire",
-            "Ruby", "Copper", "Tin", "Silver", "Nikolite", "Tungsten" };
-    public static final String[] PROCESSED = {"gemSapphire", "gemGreenSapphire", "gemRuby",
-        "ingotCopper", "ingotTin", "ingotSilver", "dustNikolite", "ingotTungsten"};
-    public static final String[] PROCESSED_NAMES = {"Sapphire Gem", "Green Sapphire Gem", "Ruby Gem",
-        "Copper Ingot", "Tin Ingot", "Silver Ingot", "Nikolite Dust", "Tungsten Ingot"};
+    public static final String[] ORES = { "Ruby", "GreenSapphire", "Sapphire",
+            "Tin", "Copper", "Silver", "Nikolite", "Tungsten" };
+    public static final String[] ORE_NAMES = { "Ruby", "Green Sapphire",
+            "Sapphire", "Tin", "Copper", "Silver", "Nikolite", "Tungsten" };
+    public static final String[] PROCESSED = { "gemRuby", "gemGreenSapphire",
+            "gemSapphire", "ingotTin", "ingotCopper", "ingotSilver",
+            "dustNikolite", "ingotTungsten" };
+    public static final String[] PROCESSED_NAMES = { "Ruby Gem",
+            "Green Sapphire Gem", "Sapphire Gem", "Tin Ingot", "Copper Ingot",
+            "Silver Ingot", "Nikolite Dust", "Tungsten Ingot" };
 
 }
