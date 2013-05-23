fef8658e10eb49e6dd9375bd81ff9023f2d741ef
Numerios <mrnumerios@gmail.com>
2013-05-23 21:55:52 +0200

    Crafting of Storage Blocks

--- common/num/numirpworld/recipe/ModCrafting.java
+++ common/num/numirpworld/recipe/ModCrafting.java
@@ -1,8 +1,35 @@
 package num.numirpworld.recipe;
 
+import num.numirpworld.block.ModBlocks;
+import num.numirpworld.item.ModItems;
+import cpw.mods.fml.common.registry.GameRegistry;
+
 public class ModCrafting {
 
     public static void init() {
+        GameRegistry.addRecipe(ModBlocks.blockStorageRuby,
+                new Object[] { "###", "###", "###", Character.valueOf('#'), 
+                ModItems.gemRuby });
+        GameRegistry.addRecipe(ModBlocks.blockStorageGreenSapphire,
+                new Object[] { "###", "###", "###", Character.valueOf('#'),
+                ModItems.gemGreenSapphire });
+        GameRegistry.addRecipe(ModBlocks.blockStorageSapphire, new Object[] {
+                "###", "###", "###", Character.valueOf('#'),
+                ModItems.gemSapphire });
+        GameRegistry.addRecipe(ModBlocks.blockStorageTin, new Object[] { "###",
+                "###", "###", Character.valueOf('#'), ModItems.ingotTin });
+        GameRegistry.addRecipe(ModBlocks.blockStorageCopper, new Object[] {
+                "###", "###", "###", Character.valueOf('#'),
+                ModItems.ingotCopper });
+        GameRegistry.addRecipe(ModBlocks.blockStorageSilver, new Object[] {
+                "###", "###", "###", Character.valueOf('#'),
+                ModItems.ingotSilver });
+        GameRegistry.addRecipe(ModBlocks.blockStorageNikolite, new Object[] {
+                "###", "###", "###", Character.valueOf('#'),
+                ModItems.dustNikolite });
+        GameRegistry.addRecipe(ModBlocks.blockStorageTungsten, new Object[] {
+                "###", "###", "###", Character.valueOf('#'),
+                ModItems.ingotTungsten });
 
     }
 }
