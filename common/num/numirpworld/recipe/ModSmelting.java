606f35e99138f2e2d14aca5df86a5ba7fb10665f
Numerios <mrnumerios@gmail.com>
2013-05-23 21:42:38 +0200

    Added ore smelting Now you can get to any itemProcessed in the game!

--- common/num/numirpworld/recipe/ModSmelting.java
+++ common/num/numirpworld/recipe/ModSmelting.java
@@ -2,10 +2,19 @@ package num.numirpworld.recipe;
 
 import net.minecraft.item.crafting.FurnaceRecipes;
 import num.numirpworld.block.ModBlocks;
+import num.numirpworld.item.ModItems;
 
 public class ModSmelting {
 
     public static void init() {
-        FurnaceRecipes.smelting().addSmelting(ModBlocks.blockOres.blockID, 0, ModBlocks.blockStorageSapphire, 0.5F);
+        FurnaceRecipes.smelting().addSmelting(ModBlocks.blockOres.blockID, 3,
+                ModItems.ingotTin, 0.5F);
+        FurnaceRecipes.smelting().addSmelting(ModBlocks.blockOres.blockID, 4,
+                ModItems.ingotCopper, 0.5F);
+        FurnaceRecipes.smelting().addSmelting(ModBlocks.blockOres.blockID, 5,
+                ModItems.ingotSilver, 0.5F);
+        FurnaceRecipes.smelting().addSmelting(ModBlocks.blockOres.blockID, 7,
+                ModItems.ingotTungsten, 0.5F);
+
     }
 }
