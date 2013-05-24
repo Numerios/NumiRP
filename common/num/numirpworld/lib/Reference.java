e75e76ad6accd613469528afa9a4a1b5bd98dfb8
Numerios <mrnumerios@gmail.com>
2013-05-24 16:07:49 +0200

    0.1.0-alpha3 TODO for 0.1.0 release: * nuggets * marble + basalt * storage block textures

--- common/num/numirpworld/lib/Reference.java
+++ common/num/numirpworld/lib/Reference.java
@@ -3,6 +3,6 @@ package num.numirpworld.lib;
 public class Reference {
     public static final String MOD_ID = "NumiRPWorld";
     public static final String MOD_NAME = "NumiRPWorld";
-    public static final String MOD_VERSION = "0.1.0-alpha2";
+    public static final String MOD_VERSION = "0.1.0-alpha3";
     public static final String TEXTURE_PATH = MOD_ID + ":";
 }
