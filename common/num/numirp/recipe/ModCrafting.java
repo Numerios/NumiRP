8b29ee10b8b6bb0b5ce208c07e7b0dcccf46a4ee
Numerios <mrnumerios@gmail.com>
2013-05-25 23:43:12 +0200

    All Decor blocks are now craftable! :)

--- common/num/numirp/recipe/ModCrafting.java
+++ common/num/numirp/recipe/ModCrafting.java
@@ -1,5 +1,6 @@
 package num.numirp.recipe;
 
+import net.minecraft.item.ItemStack;
 import num.numirp.block.ModBlocks;
 import num.numirp.item.ModItems;
 import cpw.mods.fml.common.registry.GameRegistry;
@@ -7,12 +8,14 @@ import cpw.mods.fml.common.registry.GameRegistry;
 public class ModCrafting {
 
     public static void init() {
-        GameRegistry.addRecipe(ModBlocks.blockStorageRuby,
-                new Object[] { "###", "###", "###", Character.valueOf('#'), 
-                ModItems.gemRuby });
+        GameRegistry
+                .addRecipe(
+                        ModBlocks.blockStorageRuby,
+                        new Object[] { "###", "###", "###",
+                                Character.valueOf('#'), ModItems.gemRuby });
         GameRegistry.addRecipe(ModBlocks.blockStorageGreenSapphire,
                 new Object[] { "###", "###", "###", Character.valueOf('#'),
-                ModItems.gemGreenSapphire });
+                        ModItems.gemGreenSapphire });
         GameRegistry.addRecipe(ModBlocks.blockStorageSapphire, new Object[] {
                 "###", "###", "###", Character.valueOf('#'),
                 ModItems.gemSapphire });
@@ -30,6 +33,18 @@ public class ModCrafting {
         GameRegistry.addRecipe(ModBlocks.blockStorageTungsten, new Object[] {
                 "###", "###", "###", Character.valueOf('#'),
                 ModItems.ingotTungsten });
+        GameRegistry.addRecipe(new ItemStack(ModBlocks.blockDecor, 4, 4),
+                new Object[] { "##", "##", Character.valueOf('#'),
+                        ModBlocks.blockBasalt });
+        GameRegistry.addRecipe(new ItemStack(ModBlocks.blockDecor, 4, 2),
+                new Object[] { "##", "##", Character.valueOf('#'),
+                        ModBlocks.blockMarble });
+        GameRegistry.addRecipe(new ItemStack(ModBlocks.blockDecor, 4, 5),
+                new Object[] { "##", "##", Character.valueOf('#'),
+                        ModBlocks.blockBasaltBrick });
+        GameRegistry.addRecipe(new ItemStack(ModBlocks.blockDecor, 1, 6),
+                new Object[] { "#", Character.valueOf('#'),
+                        ModBlocks.blockBasalt });
 
     }
 }
