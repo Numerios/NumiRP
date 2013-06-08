66c67d7d5fc5d893266031a8257504361fd20e3d
JBuk <jan.bukva@gmail.com>
2013-06-08 23:10:48 +0200

    Lamps overlay finally fixed!

--- common/num/numirp/client/renderer/block/BlockLampRenderer.java
+++ common/num/numirp/client/renderer/block/BlockLampRenderer.java
@@ -8,6 +8,7 @@ import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.world.IBlockAccess;
 import num.numirp.block.BlockLamp;
 import num.numirp.core.proxy.ClientProxy;
+import num.numirp.lib.BlockIDs;
 import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
 
 public class BlockLampRenderer implements ISimpleBlockRenderingHandler {
@@ -53,14 +54,50 @@ public class BlockLampRenderer implements ISimpleBlockRenderingHandler {
     }
 
     @Override
-    public boolean renderWorldBlock(IBlockAccess world, int x, int y, int z, Block block, int modelId, RenderBlocks renderer) {
+    public boolean renderWorldBlock(IBlockAccess world, int x, int y, int z, Block block, int modelId,
+            RenderBlocks renderer) {
         if (ClientProxy.renderPass == 1) {
+
+            double north = -0.03D;
+            double south = 1.03D;
+            double east = 1.03D;
+            double west = -0.03D;
+            double up = 1.03D;
+            double down = -0.03D;
+
+            int tmpBlockId;
+
+            tmpBlockId = world.getBlockId(x, y, z - 1);
+            if (tmpBlockId == BlockIDs.LAMPS_NORMAL_ACTIVE_ID || tmpBlockId == BlockIDs.LAMPS_INVERTED_ID)
+                north = 0D;
+
+            tmpBlockId = world.getBlockId(x, y, z + 1);
+            if (tmpBlockId == BlockIDs.LAMPS_NORMAL_ACTIVE_ID || tmpBlockId == BlockIDs.LAMPS_INVERTED_ID)
+                south = 1D;
+
+            tmpBlockId = world.getBlockId(x + 1, y, z);
+            if (tmpBlockId == BlockIDs.LAMPS_NORMAL_ACTIVE_ID || tmpBlockId == BlockIDs.LAMPS_INVERTED_ID)
+                east = 1D;
+
+            tmpBlockId = world.getBlockId(x - 1, y, z);
+            if (tmpBlockId == BlockIDs.LAMPS_NORMAL_ACTIVE_ID || tmpBlockId == BlockIDs.LAMPS_INVERTED_ID)
+                west = 0D;
+
+            tmpBlockId = world.getBlockId(x, y + 1, z);
+            if (tmpBlockId == BlockIDs.LAMPS_NORMAL_ACTIVE_ID || tmpBlockId == BlockIDs.LAMPS_INVERTED_ID)
+                up = 1D;
+
+            tmpBlockId = world.getBlockId(x, y - 1, z);
+            if (tmpBlockId == BlockIDs.LAMPS_NORMAL_ACTIVE_ID || tmpBlockId == BlockIDs.LAMPS_INVERTED_ID)
+                down = 0D;
+
             BlockLamp lamp = (BlockLamp) block;
             int metadata = world.getBlockMetadata(x, y, z);
             Color colors = lamp.getOverlayColor(metadata);
-            renderer.overrideBlockBounds(-0.03D, -0.03D, -0.03D, 1.03D, 1.03D, 1.03D);
+            renderer.overrideBlockBounds(west, down, north, east, up, south);
             renderer.setOverrideBlockTexture(lamp.glowTexture);
-            renderer.renderStandardBlockWithColorMultiplier(lamp, x, y, z, (float) colors.getRed() / 255, (float) colors.getGreen() / 255, (float) colors.getBlue() / 255);
+            renderer.renderStandardBlockWithColorMultiplier(lamp, x, y, z, (float) colors.getRed() / 255,
+                    (float) colors.getGreen() / 255, (float) colors.getBlue() / 255);
             renderer.clearOverrideBlockTexture();
             renderer.unlockBlockBounds();
             return true;
