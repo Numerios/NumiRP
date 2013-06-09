8bd8f84ad69b520be94b1d84496ad25de1179394
Numerios <mrnumerios@gmail.com>
2013-06-09 16:01:26 +0200

    Deleted the Crafting Handler - it serves no purpose at all (only creating dupe bugs)

--- common/num/numirp/NumiRP.java
+++ common/num/numirp/NumiRP.java
@@ -41,7 +41,6 @@ public class NumiRP {
         ModCrafting.init();
         ModSmelting.init();
 
-        proxy.initCrafting();
         proxy.initCapes();
     }
 
--- common/num/numirp/core/proxy/CommonProxy.java
+++ common/num/numirp/core/proxy/CommonProxy.java
@@ -1,13 +1,6 @@
 package num.numirp.core.proxy;
 
-import num.numirp.core.handlers.CraftingHandler;
-import cpw.mods.fml.common.registry.GameRegistry;
-
 public class CommonProxy {
-
-    public void initCrafting() {
-        GameRegistry.registerCraftingHandler(new CraftingHandler());
-    }
 
     public void initCapes() {
     }
