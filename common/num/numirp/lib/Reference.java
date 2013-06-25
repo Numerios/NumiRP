c999b7e388f27c6309969be11688d0c1dd65bf72
Numerios <mrnumerios@gmail.com>
2013-06-25 18:05:25 +0200

    Version bump to 1.0.6 (changelog below)
    * FIX: Ores are now generating in other dimensions than End, Nether and Overworld
    * FEATURE: New recipes for lamps!
    xLamp + yLumar -> yLamp
    invLamp + RS -> classicLamp
    classicLamp + RS torch -> invLamp
     xLumar + yColor -> yLumar

--- common/num/numirp/lib/Reference.java
+++ common/num/numirp/lib/Reference.java
@@ -3,7 +3,7 @@ package num.numirp.lib;
 public class Reference {
     public static final String MOD_ID = "NumiRP";
     public static final String MOD_NAME = "NumiRP";
-    public static final String MOD_VERSION = "1.0.5"; //TODO: Think of a better way to do version bumps.
+    public static final String MOD_VERSION = "1.0.6"; //TODO: Think of a better way to do version bumps.
     public static final String TEXTURE_PATH = MOD_ID + ":";
 
     public static final String CLIENT_PROXY_CLASS = "num.numirp.core.proxy.ClientProxy";
