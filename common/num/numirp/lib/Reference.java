3959d70c31db44d56880c6ddf5899eba4c9431b7
Numerios <mrnumerios@gmail.com>
2013-08-05 23:12:15 +0200

    1.1.0-alpha bump!

--- common/num/numirp/lib/Reference.java
+++ common/num/numirp/lib/Reference.java
@@ -3,7 +3,7 @@ package num.numirp.lib;
 public class Reference {
     public static final String MOD_ID = "NumiRP";
     public static final String MOD_NAME = "NumiRP";
-    public static final String MOD_VERSION = "1.0.9"; //TODO: Think of a better way to do version bumps.
+    public static final String MOD_VERSION = "1.1.0-alpha"; //TODO: Think of a better way to do version bumps.
     public static final String TEXTURE_PATH = MOD_ID + ":";
 
     public static final String CLIENT_PROXY_CLASS = "num.numirp.core.proxy.ClientProxy";
